709d390faa21c69050b1386990d90a66466c825c
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-14 12:38:27 +0900

    fix: バグ修正

--- cmds/entertainment/__init__.py
+++ cmds/entertainment/__init__.py
@@ -6,7 +6,7 @@ async def setup(bot):
   for name in listdir("cmds/entertainment"):
     if not name.startswith("."):
       try:
-        await bot.load_extension("cmds.entertainment"+name.replace(".py",""))
+        await bot.load_extension("cmds.entertainment."+name.replace(".py",""))
       except Exception as e:
         print("[Log][err]" + str(e))
       else:
