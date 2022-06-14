0dfe5af2ed267af815ae0f9eee42650595079525
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-14 11:06:07 +0900

    new: コマンド読み込み機能の追加

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ bot = commands.Bot(command_prefix='gm!',intents=intent)
 @bot.listen()
 async def on_ready():
   print("[Log]Hello " + bot.user.name)
+  for name in listdir("cmds"):
+    if not name.startswith("."):
+      try:
+        bot.load_extension(name.replace(".py",""))
+      except Exception as e:
+        print("[Log][err]" + str(e))
+      else:
+        print("[Log][load]" + name)
+  print("[Log]Complete Booting,Thank you for using " + bot.user.name)
 
 bot.run(os.environ["TOKEN"])
