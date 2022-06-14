5a1a3034ffbc3246210b8a777a5d6c4e6b6aaf38
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-14 12:45:20 +0900

    fix: 誤字修正

--- cmds/entertainment/__init__.py
+++ cmds/entertainment/__init__.py
@@ -4,7 +4,7 @@ import traceback
 
 async def setup(bot):
   for name in listdir("cmds/entertainment"):
-    if not name.startswith(("_"".")):
+    if not name.startswith(("_",".")):
       try:
         await bot.load_extension("cmds.entertainment."+name.replace(".py",""))
       except Exception as e:
