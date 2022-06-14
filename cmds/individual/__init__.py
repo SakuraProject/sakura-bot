f4e851720757990655acd89ea6c05dd033bd44ad
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-14 12:42:34 +0900

    fix: バグ修正

--- cmds/individual/__init__.py
+++ cmds/individual/__init__.py
@@ -4,7 +4,7 @@ import traceback
 
 async def setup(bot):
   for name in listdir("cmds/individual"):
-    if not name.startswith("."):
+    if not name.startswith(("_",".")):
       try:
         await bot.load_extension("cmds.individual."+name.replace(".py",""))
       except Exception as e:
