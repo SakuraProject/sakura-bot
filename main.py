0e42818809e6179de189a2f7dcca8619fc10ec0c
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-15 15:11:09 +0900

    new: MySQL接続を追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import os
+import aiomysql
 from dotenv import load_dotenv
 from aiohttp import ClientSession
 from ujson import dumps
@@ -23,6 +24,8 @@ async def on_ready():
         print("[Log][load]" + name)
   bot.session = ClientSession(loop=bot.loop, json_serialize=dumps)
   bot.Color = 0xffbdde
+  print("[Log]Connecting MySQL")
+  bot.pool = await aiomysql.create_pool(host=os.environ["MYSQLHOST"], port=int(os.environ["MYSQLPORT"]),user=os.environ["MYSQLUSER"], password=os.environ["MYSQLPASS"],db=os.environ["MYSQLDB"], loop=bot.loop,autocommit=True)
   print("[Log]Complete Booting,Thank you for using " + bot.user.name)
 
 bot.run(os.environ["TOKEN"])
