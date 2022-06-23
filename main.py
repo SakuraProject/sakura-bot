ff2ac4fd32e23b458aadf9c6af46361bfa2d90ae
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-24 07:20:30 +0900

    change: botの起動前に定義

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ bot = commands.Bot(command_prefix='gm!',intents=intent)
 @bot.listen()
 async def on_ready():
   print("[Log]Hello " + bot.user.name)
+  bot.session = ClientSession(loop=bot.loop, json_serialize=dumps)
+  bot.Color = 0xffbdde
   print("[Log]Connecting MySQL")
   bot.pool = await aiomysql.create_pool(host=os.environ["MYSQLHOST"], port=int(os.environ["MYSQLPORT"]),user=os.environ["MYSQLUSER"], password=os.environ["MYSQLPASS"],db=os.environ["MYSQLDB"], loop=bot.loop,autocommit=True)
   for name in os.listdir("cmds"):
@@ -24,8 +26,6 @@ async def on_ready():
         print("[Log][err]" + str(e))
       else:
         print("[Log][load]" + name)
-  bot.session = ClientSession(loop=bot.loop, json_serialize=dumps)
-  bot.Color = 0xffbdde
   print("[Log]Complete Booting,Thank you for using " + bot.user.name)
 
 bot.run(os.environ["TOKEN"])
