a04732779825263ddd6a067736681f1465b184e8
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-29 22:25:17 +0900

    new: スピードテストコマンドの作成

---
+++ cmds/bot/speedtest.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import speedtest
+
+class speedtest(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def speedtest(self,ctx):
+        """
+        NLang ja botサーバーのスピードテスト
+        botを動かしているサーバーのスピードテストを実行します
+        **使いかた：**
+        EVAL self.bot.command_prefix+'speedtest'
+        ELang ja
+        NLang default speedtest for bot server
+        speedtest for server to run bot
+        **How to use：**
+        EVAL self.bot.command_prefix+'speedtest'
+        ELang default
+        """
+        msg = await ctx.send("計測中、しばらくお待ちください")
+        stest = speedtest.Speedtest()
+        stest.get_best_server()
+        up = await _bot.loop.run_in_executor(None, stest.upload)
+        dl = await _bot.loop.run_in_executor(None, stest.download)
+        ebd = discord.Embed(title="speedtest",description="**ダウンロード**:\n" + str(dl/1024/1024) + "Mbps\n**アップロード**:\n" + str(up/1024/1024) + "Mbps")
+        await msg.edit(content="",embeds=[ebd])
+        
+
+async def setup(bot):
+    await bot.add_cog(speedtest(bot))
