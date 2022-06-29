a4bfbb51c2ff7248125d1517ab7f190357e97e31
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-29 22:39:10 +0900

    fix: 修正

--- cmds/bot/speedtest.py
+++ cmds/bot/speedtest.py
@@ -22,7 +22,7 @@ class speedtest(commands.Cog):
         """
         msg = await ctx.send("計測中、しばらくお待ちください")
         stest = Speedtest()
-        stest.get_best_server()
+        await self.bot.loop.run_in_executor(None,stest.get_best_server)
         up = await self.bot.loop.run_in_executor(None, stest.upload)
         dl = await self.bot.loop.run_in_executor(None, stest.download)
         ebd = discord.Embed(title="speedtest",description="**ダウンロード**:\n" + str(dl/1024/1024) + "Mbps\n**アップロード**:\n" + str(up/1024/1024) + "Mbps")
