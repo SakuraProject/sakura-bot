b06f8656e30dcb65d5ebc3325b773ba94eae4b63
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-30 18:33:47 +0900

    fix: バグ修正

--- cmds/serverutil/info.py
+++ cmds/serverutil/info.py
@@ -7,7 +7,7 @@ class infomation(commands.Cog):
         self.bot = bot
         self.user_badge = {"UserFlags.verified_bot_developer":"<:verified_bot_developer:991964080292233306>","UserFlags.early_supporter":"<:early_supporter:991963681502003230>","UserFlags.staff":"<:discord_staff:991963642729869372>","UserFlags.partner":"<:partnered_server_owner:991964149884137472>","UserFlags.hypesquad":"<:discord_HypeSquad_disc:991962182604566639>","UserFlags.bug_hunter":"<:bug_hunter:991963877770276944>","UserFlags.hypesquad_bravery":"<:discord_hypesquad_bravery_disc:991962211641741392>","UserFlags.hypesquad_brilliance":"<:discord_hypesquad_briliance_disc:991962274816331796>","UserFlags.hypesquad_balance":"<:discord_hypesquad_balance_disc:991962200879157288>"}
         self.bt = "<:discord_Bot_disc:991962236706885734>"
-        self.vbt = "<:verified_bot:991963186234413139> <:system:991963778226847814>"
+        self.vbt = "<:verified_bot:991963186234413139>"
     @commands.command(
         aliases=["ui","ユーザー情報"]
     )
