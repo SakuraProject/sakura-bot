473bd060c3451107c0fc5375a3bca86b9e407999
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-07-26 22:47:41 +0900

    update: 作りかけ。

--- cmds/individual/obj_info.py
+++ cmds/individual/obj_info.py
@@ -1,13 +1,18 @@
-# sakura bot - discord object Info commands
+# Sakura bot - discord object Info commands
 
 from discord.ext import commands
+import discord
 
 
 class ObjectInfo(commands.Cog):
-    "discordのオブジェクト情報表示コマンドでのコグです。"
+    "discordのオブジェクト情報表示コマンドのコグです。"
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+    @commands.command()
+    async def userinfo(self, ctx, arg: discord.User):
+        e = discord.Embed()
 
 
 async def setup(bot: commands.Bot) -> None:
