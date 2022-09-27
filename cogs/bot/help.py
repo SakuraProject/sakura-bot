6d6e0e004ed0701f3bf5995c0bcb011305f9ffb8
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-09-27 00:10:23 +0000

    update: help全書き直し

--- cogs/bot/help.py
+++ cogs/bot/help.py
@@ -1,193 +1,127 @@
-import importlib
 import os
+import inspect
+from unicodedata import category
 
 import discord
 from discord.ext import commands
 
-from utils import Bot
+from utils import Bot, TimeoutView
+from data.help import HELP
+
+FIRST_DESC = ("これはBotのヘルプです。下の選択メニューからカテゴリを選ぶことによりコマンドを選択できます。"
+    "これを見てもよくわからない方はサポートサーバーまでお問い合わせください。")
 
 
 class Help(commands.Cog):
     def __init__(self, bot: Bot):
-        self.bot, self.before = bot, ""
+        self.bot = bot
         self.helps = {}
 
-    @commands.command(
-        aliases=["ヘルプ"]
+    @commands.hybrid_command(
+        description="ヘルプを表示します。", aliases=["ヘルプ"]
     )
-    async def help(self, ctx: commands.Context, cmd=None, subcmd=None):
-        """
-        NLang ja ヘルプを表示します
-        あなたが今実行しているコマンドです。ヘルプを表示します
-        **使いかた：**
-        EVAL self.bot.command_prefix+'help'
-        EVAL self.bot.command_prefix+'help コマンド名'
-        ELang ja
-        NLang default SHow Help
-        Is the command you are running now
-        **How to use：**
-        EVAL self.bot.command_prefix+'help'
-        EVAL self.bot.command_prefix+'help command name'
-        ELang default
-        """
-        embed = await self.create_help(cmd, subcmd)
-        view = discord.ui.View()
-        view.add_item(CatList(self))
-        await ctx.send(embed=embed, view=view)
+    async def help(self, ctx: commands.Context, *, search_query: str | None = None):
+        embed = await self.create_help(search_query)
+        view = TimeoutView([CategoryList(self)])
+        await view.send(ctx, embed=embed, view=view)
 
-    async def create_help(self, cmd=None, subcmd=None) -> discord.Embed:
-        l = "ja"
-        sed = "これはBotのヘルプです。下の選択メニューからカテゴリを選ぶことによりコマンドを選択できます。これを見てもよくわからない方はサポートサーバーまでお問い合わせください"
-        prefix = self.bot.command_prefix if isinstance(self.bot.command_prefix, str) else "/"
-        if cmd is not None:
-            if subcmd is None:
-                comd = self.bot.get_command(cmd)
-                assert comd
-                if isinstance(comd, commands.Group | commands.HybridGroup):
-                    comds = list(comd.commands)
-                    sed = ""
-                    for gcm in comds:
-                        if gcm.callback.__doc__ is not None:
-                            doc = await self.parsedoc(gcm.callback.__doc__)
-                            try:
-                                sed = sed + prefix + comd.name + \
-                                    " " + gcm.name + " : " + \
-                                    doc["sd"][l] + "\n"
-                            except KeyError:
-                                sed = sed + prefix + comd.name + \
-                                    " " + gcm.name + " : " + \
-                                    doc["sd"]["default"] + "\n"
-                        else:
-                            sed = sed + prefix + comd.name + " " + gcm.name + " : \n"
-                elif isinstance(comd, commands.Command | commands.HybridCommand):
-                    if comd.callback.__doc__ is not None:
-                        doc = await self.parsedoc(comd.callback.__doc__)
-                        try:
-                            sed = doc["desc"][l]
-                        except KeyError:
-                            sed = doc["desc"]["default"]
-                    else:
-                        sed = "このコマンドの詳細はサポートサーバーでお問い合わせください"
+    async def create_help(self, query: str | None) -> discord.Embed:
+        desc = FIRST_DESC
+        if query:
+            # ヘルプを検索する。
+            if parfect := HELP.get(query):
+                desc = parfect
             else:
-                comd = self.bot.get_command(cmd + " " + subcmd)
-                assert comd
-                if type(comd).__name__ == "Command" or type(comd).__name__ == "HybridCommand":
-                    if comd.callback.__doc__ is not None:
-                        doc = await self.parsedoc(comd.callback.__doc__)
-                        try:
-                            sed = doc["desc"][l]
-                        except KeyError:
-                            sed = doc["desc"]["default"]
-                    else:
-                        sed = "このコマンドの詳細はサポートサーバーでお問い合わせください"
-        return discord.Embed(color=self.bot.Color, description=sed)
+                desc = "そのコマンドは見つかりませんでした。"
+        return discord.Embed(color=self.bot.Color, description=desc)
 
-    async def parsedoc(self, doc):
-        spl = doc.splitlines()
-        lang = dict()
-        res = dict()
-        sdesc = dict()
-        dol = False
-        dln = ""
-        for cmds in spl:
-            cmds = cmds.replace("    ", "")
-            cmd = cmds.split(" ")
-            if cmd[0] == "NLang":
-                dol = True
-                dln = cmd[1]
-                sdesc[dln] = cmds.replace("NLang " + dln + " ", "")
-                lang[dln] = ""
+    def get_category(self, command: commands.Command) -> str:
+        paths = inspect.getfile(command).split(os.path.sep)
+        category_name = paths[paths.index("cogs") - len(paths) + 1]
+        return category_name
+
+    def create_categories_commands(self) -> dict[str, list[str]]:
+        result = {}
+        for cmd_name in HELP.keys():
+            command = self.bot.get_command(cmd_name)
+            if not command:
                 continue
-            if cmd[0] == "ELang":
-                dol = False
-                dln = ""
-                continue
-            if dol:
-                if cmd[0] == "EVAL":
-                    lang[dln] = lang[dln] + \
-                        eval(cmds.replace("EVAL ", "")) + "\n"
-                else:
-                    lang[dln] = lang[dln] + cmds + "\n"
-        res["sd"] = sdesc
-        res["desc"] = lang
-        return res
+            category = self.get_category(command)
+            if category not in result:
+                result[category] = [cmd_name]
+            else:
+                result[category].append(cmd_name)
+        return result
+
+    def get_commands(self, cmds: list[str]) -> list[commands.Command]:
+        result = []
+        for cmd_name in cmds:
+            cmd = self.bot.get_command(cmd_name)
+            if not cmd: continue
+            result.append(cmd)
+        return result
 
 
-class CatList(discord.ui.Select):
-    def __init__(self, cog):
-        l = "ja"
+class CategoryList(discord.ui.Select):
+    def __init__(self, cog: Help):
         self.cog = cog
+
         options = []
-        for name in os.listdir("cogs"):
-            if not name.startswith((".", "_")):
-                try:
-                    name = importlib.import_module(f"cogs.{name}").name
-                    try:
-                        rname = eval(name)[l]
-                    except KeyError:
-                        rname = eval(name)["default"]
-                except AttributeError:
-                    rname = name
-                options.append(discord.SelectOption(
-                    label=name, description='', value=rname))
-        super().__init__(placeholder='', min_values=1, max_values=1, options=options)
+        for category_name in self.cog.create_categories_commands().keys():
+            options.append(discord.SelectOption(
+                label=category_name, value=category_name
+            ))
+        super().__init__(
+            placeholder='カテゴリを選択...', min_values=1,
+            max_values=1, options=options
+        )
 
     async def callback(self, interaction: discord.Interaction):
-        bot = self.cog.bot
-        sed = ""
-        cmds = list()
-        l = "ja"
-        view = discord.ui.View()
-        for c in [m for m in bot.cogs.values() if m.__module__.startswith("cogs."+self.values[0])]:
-            for cm in c.get_commands():
-                if cm.callback.__doc__ != None:
-                    doc = await self.cog.parsedoc(cm.callback.__doc__)
-                    try:
-                        sed = sed + self.cog.bot.command_prefix + \
-                            cm.name + " : " + doc["sd"][l] + "\n"
-                    except KeyError:
-                        sed = sed + self.cog.bot.command_prefix + \
-                            cm.name + " : " + doc["sd"]["default"] + "\n"
-                else:
-                    sed = sed + self.cog.bot.command_prefix + cm.name + " : \n"
-                cmds.append(cm)
-        ebd = discord.Embed(color=self.cog.bot.Color, description=sed)
-        view.add_item(self)
-        view.add_item(CmdList(cmds, self.cog))
-        await interaction.response.edit_message(embeds=[ebd], view=view)
+        # コマンド一覧を生成する
+        desc = ""
+        for cmd_name in self.cog.create_categories_commands()[self.values[0]]:
+            cmd = self.cog.bot.get_command(cmd_name)
+            assert cmd
+            if cmd.description:
+                desc += f"\n{cmd_name}: {cmd.description}"
+            else:
+                desc += f"\n{cmd_name}: {HELP[cmd_name].splitlines()[0]}"
+        embed = discord.Embed(title=self.values[0], description=desc)
+        view = TimeoutView([self, CmdList(self.values[0], self.cog)])
+        view.message = interaction.message
+        await interaction.response.edit_message(embeds=[embed], view=view)
 
 
 class CmdList(discord.ui.Select):
-    def __init__(self, cmds, cog):
+    def __init__(self, category_name: str, cog: Help):
         self.cog = cog
         options = []
-        self.cmds = cmds
-        for cm in cmds:
-            if cm.parent == None:
-                options.append(discord.SelectOption(
-                    label=self.cog.bot.command_prefix + cm.name, description='', value=cm.name))
-            else:
-                options.append(discord.SelectOption(label=self.cog.bot.command_prefix + cm.parent.qualified_name +
-                               " " + cm.name, description='', value=cm.parent.qualified_name + " " + cm.name))
-        super().__init__(placeholder='', min_values=1, max_values=1, options=options)
+        self.category_name = category_name
+        self.cmds = cog.get_commands(
+            cog.create_categories_commands()[category_name]
+        )
+        for cm in self.cmds:
+            options.append(discord.SelectOption(
+                label=cm.name, value=cm.name
+            ))
+        super().__init__(
+            placeholder='コマンドを選択...', min_values=1,
+            max_values=1, options=options
+        )
 
     async def callback(self, interaction: discord.Interaction):
         bot = self.cog.bot
         val = self.values[0]
         cmd = bot.get_command(val)
-        if type(cmd).__name__ == "Command" or type(cmd).__name__ == "HybridCommand":
-            if cmd.parent != None:
-                spl = val.split(" ")
-                ebd = await self.cog.create_help(spl[0], spl[1])
-            else:
-                ebd = await self.cog.create_help(val)
-            await interaction.response.edit_message(embeds=[ebd])
+        embed = await self.cog.create_help(val)
+        if isinstance(cmd, commands.Command | commands.HybridCommand):
+            await interaction.response.edit_message(embeds=[embed])
         else:
-            ebd = await self.cog.create_help(val)
-            view = discord.ui.View()
-            view.add_item(CatList(self.cog))
-            view.add_item(CmdList(list(cmd.commands), self.cog))
-            await interaction.response.edit_message(embeds=[ebd], view=view)
+            view = TimeoutView(
+                [CategoryList(self.cog), CmdList(self.category_name, self.cog)]
+            )
+            await interaction.response.edit_message(embeds=[embed], view=view)
+            view.message = interaction.message
 
 
 async def setup(bot: Bot):
