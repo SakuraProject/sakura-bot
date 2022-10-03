021a4bcc426ae696b291beb3eb23b0146663e931
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-10-03 04:10:03 +0000

    update: 忘れていた部分の書き直し完了。

--- cogs/serverutil/gban.py
+++ cogs/serverutil/gban.py
@@ -1,13 +1,16 @@
-from dis import disco
+# Sakura Bot - Global BAN
+
+import asyncio
+
 from discord.ext import commands
 import discord
-import asyncio
+
 from orjson import loads
 
 from utils import Bot, dumps, EmbedsButtonView
 
 
-class gban(commands.Cog):
+class Gban(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         self.gban_cache = []
@@ -35,12 +38,12 @@ class gban(commands.Cog):
         self.gban_cache = list(x[0] for x in res[0])
         self.gban_set_cache = list(x[0] for x in res[1])
 
-    @commands.group()
+    @commands.hybrid_group(description="GBAN機能です。")
     async def gban(self, ctx: commands.Context):
         if not ctx.invoked_subcommand:
             await ctx.reply("使用方法が違います。")
 
-    @gban.command()
+    @gban.command(description="GBANのオンオフを切り替えます。")
     @commands.has_permissions(administrator=True)
     @commands.guild_only()
     async def onoff(self, ctx: commands.Context, onoff: bool):
@@ -64,7 +67,7 @@ class gban(commands.Cog):
             self.gban_set_cache.append(ctx.guild.id)
         await ctx.send("Ok")
 
-    @gban.command()
+    @gban.command(with_app_command=False)
     @commands.is_owner()
     async def add(self, ctx: commands.Context, user_id: int, *, reason):
         if user_id in self.gban_cache:
@@ -75,12 +78,14 @@ class gban(commands.Cog):
         else:
             evi = []
         evif = dumps(evi)
+        await ctx.send("BANを開始します...")
 
         for guild in self.bot.guilds:
             if guild.id in self.gban_set_cache:
                 continue
             try:
-                await guild.ban(discord.Object(user_id), reason=reason + "\nFor Sakura Bot Global BAN")
+                await guild.ban(discord.Object(user_id),
+                    reason=reason + "\nFor Sakura Bot Global BAN")
                 await asyncio.sleep(1)
             except discord.NotFound:
                 return await ctx.send("ユーザーの取得に失敗しました。")
@@ -95,71 +100,51 @@ class gban(commands.Cog):
         self.gban_cache.append(user_id)
         await ctx.send("追加しました。")
 
-    @gban.command()
+    @gban.command(description="GBANされたユーザーの一覧を表示します。")
     async def list(self, ctx: commands.Context):
-        async with self.bot.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute("SELECT * FROM Gban2")
-                if (res := await cur.fetchall()) == 0:
-                    await ctx.send("gbanされた人はまだいません")
-                else:
-                    ebds: list[discord.Embed] = list()
-                    for r in res:
-                        user = self.bot.get_user(r[0])
-                        if user is None:
-                            uname = "名前が取得出来ませんでした"
-                        else:
-                            uname = user.name
-                        ev = loads(r[2])
-                        et = ""
-                        for e in ev:
-                            et = et + "[証拠](" + e + ")\n"
-                        ebd = discord.Embed(
-                            title=uname, description="userid:" + str(r[0]) + "\n" + r[1] + "\n" + et)
-                        ebds.append(ebd)
-                    await EmbedsButtonView(ebds).send(ctx)
+        await ctx.typing()
+        res = await self.bot.execute_sql(
+            "SELECT * FROM Gban2", _return_type="fetchall"
+        )
+        if not res:
+            await ctx.send("gbanされた人はまだいません")
+        else:
+            ebds: list[discord.Embed] = []
+            for row in res:
+                user = self.bot.get_user(row[0])
+                uname = getattr(user, "name", "名前が取得できませんでした。")
+                et = ""
+                for index, item in enumerate(loads(row[2])):
+                    et += f"[証拠画像{index}]({item})\n"
+                ebd = discord.Embed(
+                    title=uname,
+                    description=f"userid:{row[0]}\n{row[1]}\n{et}"
+                )
+                ebds.append(ebd)
+            await EmbedsButtonView(ebds).send(ctx)
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         if member.guild.id in self.gban_set_cache:
             return
-        async with self.bot.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute("SELECT * FROM `GbanSettings2` where `gid` = %s", (str(member.guild.id),))
-                res = await cur.fetchall()
-                if len(res) != 0:
-                    if res[0][1] == "off":
-                        return
-                await cur.execute("SELECT * FROM `gban` where `userid` = %s", (str(member.id),))
-                res = await cur.fetchall()
-                if len(res) != 0:
-                    await member.ban(reason="Sakura gbanのため")
-
-    async def input(self, ctx: commands.Context, q: str):
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-        await ctx.send(q)
-        while True:
+        if member.id not in self.gban_cache:
+            return
+        (row,) = await self.bot.execute_sql(
+            "SELECT * FROM Gban2 WHERE UserId = %s;", (member.id,),
+            _return_type="fetchone"
+        )
+        try:
+            await member.ban(reason=row[1] + "\nFor Sakura Global BAN.")
+        except discord.Forbidden:
             try:
-                message = await self.bot.wait_for('message', timeout=180.0, check=check)
-            except asyncio.TimeoutError:
-                await ctx.channel.send('入力を待機中です。キャンセルする場合は「キャンセルする」と送ってください')
-            else:
-                if message.content == "キャンセルする":
-                    raise SyntaxError()
-                await ctx.channel.send("入力を受け付けました。確定する場合は「ok」と送って下さい。やり直す場合は「修正」と送ってください")
-                while True:
-                    try:
-                        message1 = await self.bot.wait_for('message', timeout=180.0, check=check)
-                    except asyncio.TimeoutError:
-                        await ctx.channel.send('タイムアウトしました。入力をやりなおしてください')
-                        break
-                    else:
-                        if message1.content == "ok":
-                            return message
-                        elif message1.content == "修正":
-                            break
+                await member.guild.owner.send(  # type: ignore
+                    f"GlobalBANされたユーザー: {member.mention}が"
+                    f"{member.guild.name}に入室しましたがBANできませんでした。"
+                    "権限を確認してください。"
+                )
+            except (AttributeError, discord.HTTPException):
+                pass
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(gban(bot))
+    await bot.add_cog(Gban(bot))
