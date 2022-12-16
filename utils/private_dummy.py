5eb8f7f06f4b7319f6840ba1c72ccef095551580
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-12-16 01:53:59 +0000

    new: private_functions

---
+++ utils/private_dummy.py
@@ -0,0 +1,7 @@
+# Sakura Bot Utils - Alternative Private Functions
+
+class automod:
+    @staticmethod
+    def raidcheck(cog, member) -> bool:
+        "メンバーがレイドかどうかチェックします。"
+        return False
